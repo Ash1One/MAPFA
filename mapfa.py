e586bc2e84027246cbf7bef4260064b622c0a5f3
huanghao <huanghao_2013@qq.com>
2020-10-19 22:11:24 +0800

    append read_QC module

---
+++ mapfa.py
@@ -0,0 +1,6 @@
+import os
+import argparse
+
+
+def main ():
+    
---
+++ read_QC.py
@@ -0,0 +1,44 @@
+from multiprocessing import Pool
+import time
+import os
+import sys
+import subprocess
+
+
+# default parameters
+threads = 1
+
+def safeMkdir(dirName):
+    work_dir = os.getcwd()
+    if dirName in os.listdir():
+        print ("{} has existed.".format(dirName))
+        return dirName
+    os.mkdir(os.path.join(work_dir, dirName), 0o755)
+    print ("Create {}".format(dirName))
+    return dirName
+
+def getReadsFile():
+    return sys.argv[1:]
+
+def runFastqc(threads, fastqFile, outdir):
+    cmd = " ".join(["fastqc", "-o", outdir, "-f", "fastq", "-t", str(threads), fastqFile])
+    subprocess.run(cmd, check=True, shell=True)
+
+def main():
+    lreads_file = getReadsFile()
+    dpreQC_report = safeMkdir('preQC_report')
+    processes = len(lreads_file)
+    p = Pool(processes)
+    for reads_file in lreads_file:
+        p.apply_async(runFastqc, args=(threads, reads_file, dpreQC_report))
+    print('Waiting for all subprocesses done...')
+    p.close()
+    p.join()
+    print('All fastqc done.')
+
+if __name__ == "__main__":
+    start_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+    print("Read quality control start:\n{}".format(start_time))
+    main()
+    end_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+    print("Read quality control end:\n{}".format(end_time))
