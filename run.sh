7459b053c6bc21e3cd7422306919392a09f560af
Ashone1 <506850283@qq.com>
2022-03-05 23:27:27 +0800

    add kraken and megahit

---
+++ run.sh
@@ -0,0 +1,2 @@
+cluster="bsub -q high -n {threads} -J meta.{wildcards.sample}.{rule} -R \"span[hosts=1]\" -o LOG/{wildcards.sample}/{rule}.out -e {wildcards.sample}/{rule}.err"
+snakemake --nolock -s mapfa.snakemake --latency-wait 45 -k -j 9999 --cluster "$cluster" $@
