3e60b48420da89c753fd85e520099781e000bf78
Ashone1 <506850283@qq.com>
2022-03-05 23:31:57 +0800

    add outdir

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+outdir=$1
 cluster="bsub -q high -n {threads} -J meta.{wildcards.sample}.{rule} -R \"span[hosts=1]\" -o LOG/{wildcards.sample}/{rule}.out -e {wildcards.sample}/{rule}.err"
-snakemake --nolock -s mapfa.snakemake --latency-wait 45 -k -j 9999 --cluster "$cluster" $@
+snakemake --nolock -s mapfa.snakemake --latency-wait 45 -k -j 9999 --cluster "$cluster" -C outdir=${outidr} $@
